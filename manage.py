1c1db45a13df5d6d875c013ab917d4a75b0d9917
GalaXYGong <148594926+GalaXYGong@users.noreply.github.com>
2024-05-02 10:17:59 -0700

    add a new question to the database, "fill in the blank" question

--- manage.py
+++ manage.py
@@ -23,29 +23,51 @@ def get_meaning_phone(word):
         for word in data:
             if word["word"] == my_word:
                 my_word_meaning = word["correct"]
-                my_word_incorrect_meaning_list = word["incorrect"]
+                my_word_incorrect_list = word["incorrect"]
                 my_word_example = word["example"]
                 break
-        return my_word,my_word_phonetics,my_word_meaning,my_word_incorrect_meaning_list,my_word_example
+        return my_word,my_word_phonetics,my_word_meaning,my_word_incorrect_list,my_word_example
 
 if __name__ == "__main__":
+    # an empty list to store the words themselves
+    list_of_words = []
     # an empty list to store the processed words (objects of Word class)
     list_of_words_processed = []
+    # an empty list to store the processed words with blanks (objects of Word class)
+    list_of_words_blank = []
     # get the words we have in the json file, this words are not processed, with only the word, and 4 meanings, 1 correct and 3 incorrect
     with open("data.json","r",encoding="utf-8") as file:
         data = json.load(file)
         for word in data:
+            list_of_words.append(word["word"])
+        for word in data:
             word_tuple = get_meaning_phone(word["word"])
             # make it into an object
-            word_for_question = Word(word_tuple[0],word_tuple[1],word_tuple[2],word_tuple[3],word_tuple[4]).__dict__
-            list_of_words_processed.append(word_for_question)
+            word_for_question = Word(word_tuple[0],word_tuple[1],word_tuple[2],word_tuple[3],word_tuple[4])
+            # make it into a dictionary for json
+            word_for_question_dictionary = word_for_question.__dict__
+            # append the dictionary into the list for json
+            list_of_words_processed.append(word_for_question_dictionary)
+
+            # second type of question, fill in the blank
+            # make a sentence with the word picked out, and replace the word with blank
+            word_blank = Word(word_tuple[0],word_tuple[1],word_tuple[2],word_tuple[3],word_tuple[4])
+            # randomly choose 3 words from the list of words to be the incorrect answers
+            Word.choose_word(word_blank,list_of_words)
+            list_of_words_blank.append(word_blank.__dict__)
+    
+    
     # write the object into a json file
     database_path = Path(f'./database')
     if not database_path.exists():
         Path(f'./database').mkdir()
-    database_file = database_path.joinpath('my_json.json')
-    with open(database_file, 'w') as file:
+    database_explain_question = database_path.joinpath('my_json.json')
+    with open(database_explain_question , 'w') as file:
         json.dump(list_of_words_processed,file, indent=4)
+
+    database_fill_question = database_path.joinpath('fill_in_the_blank.json')
+    with open(database_fill_question, 'w') as file:
+        json.dump(list_of_words_blank,file, indent=4)
 
 
 """
--- model.py
+++ model.py
@@ -1,9 +1,22 @@
+import random
 class Word:
-    def __init__(self,word,phonetics,correct_meaning,incorrect_meaning_list,word_example):
+    def __init__(self,word,phonetics,correct,incorrect_list,word_example):
         self.word = word
         self.phonetics = phonetics
-        self.meaning = correct_meaning
-        self.incorrect_meaning_list = incorrect_meaning_list
+        self.correct = correct
+        self.incorrect_list = incorrect_list
         self.word_example = word_example
     def __str__(self):
-        return f'{self.word} {self.phonetics} {self.meaning} {self.incorrect_meaning_list} {self.word_example}'
+        return f'{self.word} {self.phonetics} {self.correct} {self.incorrect_list} {self.word_example}'
+    def choose_word(self,data):
+        self.incorrect_list = random.choices(data, k=3)
+        self.correct = self.word
+        list_of_word = self.word_example.split(" ")
+        for i in list_of_word:
+            # find the word in the sentence
+            if i == self.word:
+                index = list_of_word.index(i)
+                # replace the word with blank of the same length
+                list_of_word = list_of_word[:index-1] + ["_" * len(self.word)] + list_of_word[index+1:]
+        self.word_example = " ".join(list_of_word)
+        
