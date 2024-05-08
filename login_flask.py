8ace3869158da222f290d80b103eda0c7a114a6a
a0a912 <64562850+a0a912@users.noreply.github.com>
2024-05-08 12:33:22 -0700

    Managed to connect home page, ranking/scores page, to log in

--- login_flask.py
+++ login_flask.py
@@ -1,20 +1,22 @@
 # a simple login page using flask for testing the usermod.py
-from flask import Flask, render_template, url_for, request, redirect
-from user_crud_func import auth, sign_up, select_id, select_all
-import sqlite3
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, request, redirect, session
+from user_crud_func import auth, sign_up, select_all
+import secrets 
 
-#Homepage thing
-@app.route('/homepage')
-def homepage():
-    return render_template("HomePage.html")
+app = Flask(__name__)
+app.secret_key = "d4413d05138d1fa03489e233df6aca24"
 
 # page of login when you open the website 127.0.0.1:8888/
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
+    username = session.get('username')
+    return render_template("home.html", username=username)
+
+@app.route('/login', methods=['GET'])
+def login_page():
     return render_template("login.html")
 
-@app.route('/register')
+@app.route('/register', methods=['GET'])
 def register_page():
     return render_template("register.html")
 
@@ -27,8 +29,9 @@ def login():
     result = auth(username, password)
     # print(result)
     if result[0]:
+        session['username'] = username
         # if the user is authenticated then redirect to the home page with the username
-        return render_template('home.html', username=username)
+        return redirect(url_for('home'))
     # if the user is not authenticated then redirect to the login page
     return redirect(url_for('home'))
 
@@ -39,16 +42,35 @@ def register():
     password = request.form.get('password')
     result = sign_up(username, password)
     if result[0]:
-        return redirect(url_for('home'))
+        return redirect(url_for('login_page'))
     return redirect(url_for('register_page'))
 
-#Read database.db get scores for everyone and send them to scores.html. Essentially a "leaderboard".
-@app.route('/scores')
-def scores():
+@app.route('/auth/logout', methods=['POST'])
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+@app.route('/quiz', methods=['GET'])
+def quiz():
+    username = session.get('username')
+    return render_template("quiz.html", username=username)
+
+@app.route('/quiz/submit', methods=['POST'])
+def submit():
+    return 
+
+@app.route('/ranking', methods=['GET'])
+def ranking():
     scores = select_all("account", "username, score")
-    #Sort scores by descending order based on score[1]
-    scores.sort(key=lambda x: x[1], reverse=True)
-    return render_template('scores.html', scores=scores)
+    return render_template("scores.html", scores=scores)
+
+@app.route('/profile', methods=['GET'])
+def profile():
+    username =session.get('username')
+    return render_template("profile.html", username=username)
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=8888) # 端口8888
