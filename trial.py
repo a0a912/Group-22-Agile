f60ef86f50414bdf7fb04ab74f9161d260dec795
a0a912 <64562850+a0a912@users.noreply.github.com>
2024-05-08 10:46:29 -0700

    Updated gitignore added scores

--- trial.py
+++ trial.py
@@ -3,6 +3,6 @@
 
 from user_crud_func import update_score, read_question_account
 # you can register a new user and update the score, then change the user name or id to test the function
-update_score("admin", 100, ["1","2","3"], ["4","5","6"])
-update_score(1, 100, ["1","2","3"], ["4","5","6"])
-read_question_account(1) 
+update_score("ahmed", 100, ["1","2","3"], ["4","5","6"])
+update_score(2, 100, ["1","2","3"], ["4","5","6"])
+read_question_account(2) 
