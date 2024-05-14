9967d5526e9f1422a44b5a3b8fac5f3747e06551
GalaXYGong <148594926+GalaXYGong@users.noreply.github.com>
2024-05-14 12:46:49 -0700

    try unittest

---
+++ test/test_userdb.py
@@ -0,0 +1,10 @@
+import manage
+import unittest
+def test_return_all_secure_question():
+    # get all the secure questions from the database
+    # secure_questions = manage.return_all_secure_question()
+    print("All secure questions are:")
+    # check if the secure questions are the same as the secure questions in the secure_question_list
+    # assert secure_questions == secure_question_list.secure_questions
+    print("All secure questions are the same")
+    # return secure_questions
