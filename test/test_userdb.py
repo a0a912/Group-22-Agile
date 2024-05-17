4f048829f1e62f34a7458f42a420f80068e0da2a
GalaXYGong <148594926+GalaXYGong@users.noreply.github.com>
2024-05-16 18:21:53 -0700

    test

--- test/test_userdb.py
+++ test/test_userdb.py
@@ -8,11 +8,11 @@ def test_return_all_secure_question():
     assert secure_questions == database.secure_question_list.secure_questions
     print("All secure questions are the same")
     # return secure_questions
-# def test_check_secure_question():
-#     user_crud_func.sign_up("eddie", "eddie123", "What is your favorite color?",'red',"What is your favorite food?","pizza")
-#     assert user_crud_func.check_secure_question("eddie", ["What is your favorite color?","What is your favorite food?"], ["red","pizza"]) == True
-#     assert user_crud_func.check_secure_question("eddie", ["What is your favorite color?","What is your favorite food?"], ["blue","pizza"]) == False
-#     assert user_crud_func.check_secure_question("eddie", ["What is your favorite?","What is your favorite food?"], ["red","pizza"]) == False
+def test_check_secure_question():
+    user_crud_func.sign_up("eddie", "eddie123", "What is your favorite color?",'red',"What is your favorite food?","pizza")
+    # assert user_crud_func.check_secure_question("eddie", ["What is your favorite color?","What is your favorite food?"], ["red","pizza"]) == True
+    assert user_crud_func.check_secure_question("eddie", ["What is your favorite color?","What is your favorite food?"], ["blue","pizza"]) == False
+    assert user_crud_func.check_secure_question("eddie", ["What is your favorite?","What is your favorite food?"], ["red","pizza"]) == False
 
 # def test_show_secure_question():
 #     user_crud_func.sign_up("eddie", "eddie123", "What is your favorite color?",'red',"What is your favorite food?","pizza")
